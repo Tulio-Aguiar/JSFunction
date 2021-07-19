1dd1d53ba1c7e1bba3c00c2b113093d60fe8665d
Tulio-Aguiar <axeaguiar@hotmail.com>
2021-07-19 15:31:18 +0200

    JavaSriptFunction

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+function newSome(a) {
+    console.log(a)
+    console.log('Hello World')
+    console.log("1+3 = " + a)
+    console.log(32 + a)
+}
+
+newSome(1 + 3)
+
+
+//
+
+function intro2(paramName) {
+    let varName = 'Supercoder'
+    console.log(paramName)
+    console.log("Hi, " + varName + " Mein Name ist " + paramName + " .")
+
+}
+
+intro2('Tulio')
+
+
+function intro3(paramName) {
+    let varName = 'Supercoder'
+    let varMeinName = 'Túlio.'
+    let varStadt = 'Konstanz'
+    let varAlter = '33'
+    console.log(paramName)
+    console.log("Hi, " + varName + ". Mein Name ist " + varMeinName + "Ich bin " + varAlter + " Jahre alt. Ich komme aus " +
+        varStadt + ".")
+
+}
+
+intro3('Konstanz', '33')
+
+
+
+
+function myFunction(p1, p2) {
+    console.log(p1 * p2)
+    console.log(p1 * p2 * p2)
+
+}
+
+myFunction(4, 3)
+
+function myFunction(p1, p2) {
+    console.log(p1 * p2)
+    console.log(p1 * p2 * p2)
+
+}
+
+myFunction(10, 2)
+
+function myFunction(p1, p2) {
+    console.log(p1 * p2)
+    console.log(p1 * p2 * p2)
+
+}
+
+myFunction(30, 20)
+
+
+function myFunction(p1, p2) {
+    console.log(p1 * p2)
+    console.log(p1 * p2 * p2)
+    console.log(p1 / p2)
+
+}
+
+myFunction(100, 100)
+
+function myFunction(p1, p2) {
+    console.log(p1 * p2)
+    console.log(p1 * p2 * p2)
+    console.log(p1 / p2)
+
+}
+
+myFunction(5, 0)
+
+function myFunction(p1, p2) {
+    console.log(p1 * p2)
+    console.log(p1 / p2)
+
+}
+
+myFunction(45, 173)
+
+function myFunction(p1, p2) {
+    console.log(p1 * p2)
+    console.log(p1 * p2 * p2)
+    console.log(p1 / p2)
+
+}
+
+myFunction(1, 1000)
