b21a451a40cbc0d98ae2a2bf555e61935c9bdadf
Tulio-Aguiar <axeaguiar@hotmail.com>
2021-07-19 15:51:20 +0200

    Document.write

---
+++ assets/js/document.js
@@ -0,0 +1,10 @@
+document.write("<br>");
+document.write("Hello World");
+document.write("<p> Tenha um bom dia</p>");
+
+var myText = 'Hello again, bitch.'
+console.log(myText)
+
+let vorName = 'Tulio'
+let nachName = 'Aguiar'
+console.log(vorName + ' ' + nachName)
